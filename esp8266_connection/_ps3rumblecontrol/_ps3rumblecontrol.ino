00b5ad09019706e776d46f44867159a3eb541f39
Waynedemon <kloster.maxim@web.de>
2017-01-19 09:40:31 +0100

    not only wemos anymore

--- esp8266_connection/_ps3rumblecontrol/_ps3rumblecontrol.ino
+++ esp8266_connection/_ps3rumblecontrol/_ps3rumblecontrol.ino
@@ -1,19 +1,25 @@
 String inString = "";
-
+#define D5 5
+#define D6 6
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   //Serial.setTimeout(20);
   pinMode(D5, OUTPUT);
+  pinMode(D6, OUTPUT);
   pinMode(A0, INPUT);
 
+  analogWrite(D5, 0);   
+  analogWrite(D6, 0);   
+
   while (!Serial) {}
+  Serial.print("test\n");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 if(Serial.available() <= 0) {return;}
-  //Serial.print("test\n"); 
+  // 
 
   while (Serial.available() > 0) {
     int inChar = Serial.read();
@@ -24,11 +30,13 @@ if(Serial.available() <= 0) {return;}
     }
     // if you get a newline, print the string,
     // then the string's value:
-    if (inChar == '\n') {
+    if (inChar == '\n' || inChar == '$') {
       int pwm = inString.toInt();
-      analogWrite(D5, pwm);      
+      analogWrite(D5, pwm);   
+      analogWrite(D6, pwm);      
       // clear the string for new input:
       inString = "";
+      Serial.print(pwm); 
     }
   }
   
